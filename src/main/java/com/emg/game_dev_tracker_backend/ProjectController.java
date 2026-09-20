65a1c0c894aea08cb9547ad7a6c33c05aa252a2d
Elizabeth <152108764+emge01@users.noreply.github.com>
2026-09-20 18:53:10 +0100

    PATCH project endpoint implemented

--- src/main/java/com/emg/game_dev_tracker_backend/ProjectController.java
+++ src/main/java/com/emg/game_dev_tracker_backend/ProjectController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
@@ -26,16 +28,46 @@ class ProjectController {
     }
     // end::get-aggregate-root[]
 
-    @PostMapping("/projects")
-    Project newProject(@RequestBody Project newProject) {
-        return repository.save(newProject);
-    }
-
     // Single item
 
     @GetMapping("/projects/{id}")
     Project one(@PathVariable Long id) {
         return repository.findById(id).orElseThrow(() -> new ProjectNotFoundException(id));
+    }
+
+    @PostMapping("/projects")
+    Project newProject(@RequestBody Project newProject) {
+        return repository.save(newProject);
+    }
+
+    @PutMapping("/projects/{id}")
+    Project replaceProject(@RequestBody Project newProject, @PathVariable Long id) {
+        return repository.findById(id).map(project -> {
+            project.setTitle(newProject.getTitle());
+            project.setEngine(newProject.getEngine());
+            project.setPlatforms(newProject.getPlatforms());
+            project.setStatus(newProject.getStatus());
+            return repository.save(project);
+        }).orElseThrow(() -> new ProjectNotFoundException(id));
+    }
+
+    @PatchMapping("/projects/{id}")
+    Project updateProjectDetails(@RequestBody Project newProject, @PathVariable Long id) {
+        return repository.findById(id).map(project -> {
+            if (newProject.getTitle() != null) {
+                project.setTitle(newProject.getTitle());
+            }
+            if (newProject.getEngine() != null) {
+                project.setEngine(newProject.getEngine());
+            }
+            if (newProject.getPlatforms() != null) {
+                project.setPlatforms(newProject.getPlatforms());
+            }
+            if (newProject.getStatus() != null) {
+                project.setStatus(newProject.getStatus());
+            }
+            return repository.save(project);
+        }).orElseThrow(() -> new ProjectNotFoundException(id));
     }
 
     @DeleteMapping("/projects/{id}")
